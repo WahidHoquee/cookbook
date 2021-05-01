e7813f36a881c1d45ccd2bb369829bb52d634abc
WahidHoquee <wahidhoquee@gmail.com>
2021-05-02 00:26:01 +0600

    Done Project

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'fcec6957-3c0e-4945-aa8a-72005ef518ee';
+export const MODAL_CLOSE_SEC = 2.5;
